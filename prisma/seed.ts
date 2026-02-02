0e77713f5cd76f537885d6288fa2af62d6d91bfe
devZenta <hugo.ghesquier59@gmail.com>
2026-02-02 22:21:56 +0100

    feat: setup prisma with postgresql database

---
+++ prisma/seed.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+	connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({
+	adapter,
+});
+
+async function main() {
+	await prisma.user.createMany({
+		data: [
+			{ id: crypto.randomUUID(), email: "alice@example.com", name: "Alice" },
+			{ id: crypto.randomUUID(), email: "bob@example.com", name: "Bob" },
+			{
+				id: crypto.randomUUID(),
+				email: "charlie@example.com",
+				name: "Charlie",
+			},
+			{ id: crypto.randomUUID(), email: "diana@example.com", name: "Diana" },
+			{ id: crypto.randomUUID(), email: "eve@example.com", name: "Eve" },
+			{ id: crypto.randomUUID(), email: "frank@example.com", name: "Frank" },
+			{ id: crypto.randomUUID(), email: "grace@example.com", name: "Grace" },
+			{ id: crypto.randomUUID(), email: "henry@example.com", name: "Henry" },
+			{
+				id: crypto.randomUUID(),
+				email: "isabella@example.com",
+				name: "Isabella",
+			},
+			{ id: crypto.randomUUID(), email: "jack@example.com", name: "Jack" },
+		],
+		skipDuplicates: true,
+	});
+
+	console.log("Seed data inserted!");
+}
+
+main()
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
