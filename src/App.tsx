54490bfab698d7ce8dba99d89af245f63dcbd99c
GARATONCODE <kalilnx666@gmail.com>
2026-02-02 12:15:04 +0100

    feat: refactor App component to implement routing and remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home/page';
+import Header from './components/Header';
+import CreateRecipe from './pages/CreateRecipe/page';
+import Catalogue from './pages/Catalogue/page';
+import ShoppingList from './pages/ShoppingList/page';
 
 function App() {
-	const [count, setCount] = useState(0);
+  return (
+    <Router>
+      <Header logoUrl="/logo.png" />
 
-	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank" rel="noopener">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noopener">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button type="button" onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	);
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ShoppingList" element={<ShoppingList />} />
+        <Route path="/Catalogue" element={<Catalogue />} />
+        <Route path="CreateRecipe" element={<CreateRecipe />} />
+
+
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
