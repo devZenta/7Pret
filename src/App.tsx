b8bc4161ac06747342921877b9bdce2082d22120
GARATONCODE <kalilnx666@gmail.com>
2026-02-03 10:03:12 +0100

    feat: reorganize imports and add RecipeDetail route in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home/page';
-import Header from './components/Header';
-import CreateRecipe from './pages/CreateRecipe/page';
-import Catalogue from './pages/Catalogue/page';
-import ShoppingList from './pages/ShoppingList/page';
+import {
+	Navigate,
+	Route,
+	BrowserRouter as Router,
+	Routes,
+} from "react-router-dom";
+import Header from "./components/Header";
+import Catalogue from "./pages/Catalogue/page";
+import RecipeDetail from "./pages/Catalogue/recipedetail";
+import CreateRecipe from "./pages/CreateRecipe/page";
+import Home from "./pages/Home/page";
+import ShoppingList from "./pages/ShoppingList/page";
 
 function App() {
-  return (
-    <Router>
-      <Header logoUrl="/logo.png" />
+	return (
+		<Router>
+			<Header logoUrl="/logo.png" />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/ShoppingList" element={<ShoppingList />} />
-        <Route path="/Catalogue" element={<Catalogue />} />
-        <Route path="CreateRecipe" element={<CreateRecipe />} />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/ShoppingList" element={<ShoppingList />} />
+				<Route path="/Catalogue" element={<Catalogue />} />
+				<Route path="CreateRecipe" element={<CreateRecipe />} />
+				<Route path="/recipe/:id" element={<RecipeDetail />} />
 
-
-        <Route path="*" element={<Navigate replace to="/" />} />
-      </Routes>
-    </Router>
-  );
+				<Route path="*" element={<Navigate replace to="/" />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
