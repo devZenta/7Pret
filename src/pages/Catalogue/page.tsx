b51e74abdd553e9851b291c74fa146d4143f8f8d
GARATONCODE <kalilnx666@gmail.com>
2026-02-03 10:03:30 +0100

    feat: enhance Catalogue page with recipe listing and navigation links

--- src/pages/Catalogue/page.tsx
+++ src/pages/Catalogue/page.tsx
@@ -1,10 +1,58 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import recipesData from "../../data/recipes.json";
+
+interface RecipeProps {
+	id: number;
+	name: string;
+	cuisine: string;
+	Time: number;
+	image: string;
+	difficulty: string;
+	servings: number;
+}
+
 const Catalogue = () => {
-    return (
-        <div style={{ padding: '2rem', textAlign: 'center' }}>
-            <h2>Bienvenue sur le Catalogue</h2>
-            <p>Le contenu de votre page s'affiche ici.</p>
-        </div>
-    );
+	const recipes: RecipeProps[] = recipesData as unknown as RecipeProps[];
+
+	return (
+		<div>
+			<h2>Bievenue sur le Catalogue</h2>
+			<p>Voici la liste de nos recettes :</p>
+
+			<div
+				style={{
+					display: "grid",
+					gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr",
+					gap: "20px",
+				}}
+			>
+				{recipes.map((recipe) => (
+					<Link
+						to={`/recipe/${recipe.id}`}
+						key={recipe.id}
+						style={{
+							border: "1px solid #ccc",
+							padding: "10px",
+							borderRadius: "8px",
+							textDecoration: "none",
+						}}
+					>
+						<h3>{recipe.name}</h3>
+						<p>{recipe.cuisine}</p>
+						<p>
+							Temps: {recipe.Time} min ........ Pers: {recipe.servings}
+						</p>
+						<img
+							src={recipe.image}
+							alt={`Difficulté: ${recipe.difficulty}`}
+							style={{ maxWidth: "100%" }}
+						/>
+					</Link>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Catalogue;
--- src/pages/CreateRecipe/page.tsx
+++ src/pages/CreateRecipe/page.tsx
@@ -1,10 +1,10 @@
 const CreateRecipe = () => {
-    return (
-        <div style={{ padding: '2rem', textAlign: 'center' }}>
-            <h2>Bienvenue sur Create Recipe</h2>
-            <p>Le contenu de votre page s'affiche ici.</p>
-        </div>
-    );
+	return (
+		<div style={{ padding: "2rem", textAlign: "center" }}>
+			<h2>Bienvenue sur Create Recipe</h2>
+			<p>Le contenu de votre page s'affiche ici.</p>
+		</div>
+	);
 };
 
 export default CreateRecipe;
