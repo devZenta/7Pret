f8eb493dbc421c8d938b39829f4d3de2f0f3d62c
GARATONCODE <kalilnx666@gmail.com>
2026-02-03 10:06:42 +0100

    feat: refactor Header component and improve CSS formatting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,48 +1,48 @@
-import React from 'react';
-
-
+import type React from "react";
 
 interface HeaderProps {
-    logoUrl: string;
+	logoUrl: string;
 }
 
-const Header: React.FC<HeaderProps> = ({ logoUrl }) => {
-
-    return (
-        <header style={styles.header}>
-            <div className="logo-container">
-                <img src={logoUrl = './public/logo.png'} alt="Logo" style={styles.logo} />
-            </div>
-            <nav>
-                <ul style={styles.navList}>
-                    <li><a href="/">Accueil</a></li>
-                    <li><a href="/ShoppingList">Panier</a></li>
-                    <li><a href="/Recette Certifié">Recette Certifié</a></li>
-                    <li><a href="/Catalogue">Catalogue</a></li>
-                    <li><a href="/CreateRecipe">Creer une Recette+</a></li>
-
-
-
-                </ul>
-            </nav>
-        </header>
-    );
+const Header: React.FC<HeaderProps> = ({ logoUrl = "./public/logo.png" }) => {
+	return (
+		<header style={styles.header}>
+			<div className="logo-container">
+				<img src={logoUrl} alt="Logo" style={styles.logo} />
+			</div>
+			<nav>
+				<ul style={styles.navList}>
+					<li>
+						<a href="/">Accueil</a>
+					</li>
+					<li>
+						<a href="/ShoppingList">Panier</a>
+					</li>
+					<li>
+						<a href="/Recette Certifié">Recette Certifié</a>
+					</li>
+					<li>
+						<a href="/Catalogue">Catalogue</a>
+					</li>
+					<li>
+						<a href="/CreateRecipe">Creer une Recette+</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 };
 
 const styles = {
-    header: { display: 'flex', justifyContent: 'space-between', padding: '1rem', background: '#eee2dfff', color: '#562b2bff' },
-    logo: { width: '200px', marginRight: '10px' },
-    navList: { display: 'flex', listStyle: 'none', gap: '20px' }
+	header: {
+		display: "flex",
+		justifyContent: "space-between",
+		padding: "1rem",
+		background: "#eee2dfff",
+		color: "#562b2bff",
+	},
+	logo: { width: "200px", marginRight: "10px" },
+	navList: { display: "flex", listStyle: "none", gap: "20px" },
 };
 
-
-
-
-
-
-
-export default Header
-
-
-
-
+export default Header;
